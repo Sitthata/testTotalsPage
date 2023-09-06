76395c226ef820964b09ff865e371e75d123cd9c
Sitthata <aprils96950@gmail.com>
2023-09-06 22:45:23 +0700

    finished totalPage function

--- totalPages.js
+++ totalPages.js
@@ -2,5 +2,9 @@ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
   //code here
+  // Argument Validation
+  if(arrayItems == null) return undefined;
+  if(rowsPerPage == null || rowsPerPage <= 0) return 1;
+  return Math.ceil(arrayItems.length / rowsPerPage);
 }
 module.exports = totalPages
